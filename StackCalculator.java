c1328a5b70766084973336e33266eba17dfd18d9
yugal07 <yashsadhwani544@gmail.com>
2025-06-02 02:23:12 +0530

    Stack Calculator

---
+++ StackCalculator.java
@@ -0,0 +1,57 @@
+import java.util.Stack;
+
+public class StackCalculator {
+    private Stack<Integer> stack;
+
+    public StackCalculator(){
+        this.stack = new Stack<>();
+    }
+
+    public void push(int number){
+        stack.push(number);
+        System.out.println("Pushed " + number);
+    }
+    public int add(){
+        if(stack.size() < 2){
+            System.out.println("Not Enough elements to add");
+            return -1;
+        }
+        int a = stack.pop();
+        int b = stack.pop();
+
+        int result = a + b;
+        stack.push(result);
+        System.out.println(result);
+        return result;
+    }
+    public int multiply(){
+        if(stack.size() < 2){
+            System.out.println("Not enough Elements to multiply");
+
+            return -1;
+        }
+        int a = stack.pop();
+        int b = stack.pop();
+
+        int result = a * b;
+        stack.push(result);
+        System.out.println(result);
+        return result;
+    }
+    public void printStack(){
+        System.out.println("Current Stack : " + stack);
+    }
+
+    public static void main(String[] args) {
+        StackCalculator stack = new StackCalculator();
+
+        stack.push(2);
+        stack.add();
+        stack.push(5);
+        stack.add();
+        stack.printStack();
+        stack.multiply();
+        stack.push(8);
+        stack.multiply();
+    }
+}
